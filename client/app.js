26eecba1024a8d3b8c69a5e2cdaf39930c47c9b2
rrobinson18 <robinson.rakesha@gmail.com>
2019-03-30 23:55:48 -0400

    Chirper now working

--- client/app.js
+++ client/app.js
@@ -1,35 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let submitBtn = document.getElementById('submit-btn');
     let chirpField = document.getElementById('chirp-list');
-    let editSubmit = document.getElementById('editsubmit');
-    
+    let editSubmit = document.getElementById('editSubmit');
+
 
     function loadChirps() {
 
-        fetch('/api/chirps', function(data) {
-            console.log(data)
-        })
-        .done(function(data) {
-
-            let numberOfChirps = data.nextid;
-            for (var i = 0; i < numberOfChirps; i++) {
-                try{
-                    createElement(data[i].name, data[i].message, i);
+        fetch('/api/chirps')
+            .then(res => res.json())
+            .then(data => {
+                delete data.nextid;
+                for(let key in data) {
+                    createElement(data[key].user, data[key].text, key);
                 }
-                catch(error) {}
-            } 
-        });
+            });
     }
 
     function postChirp(name, message) {
-        $.post('/api/submit', {'name': name, 'message': message}
-        ).done(function() {
-            $.get('/api/chirps', function(data) {
-
-            }).done(function(data) {
-                let currentid = data.nextid - 1;
-                createElement(name, message, currentid)
-            })
+        $.post('/api/chirps', {user : name, text : message })
+        .then(() => {
+             $('#chirp-list').empty();
+            loadChirps();
         })
     }
 
@@ -40,75 +31,86 @@ document.addEventListener('DOMContentLoaded', function() {
         let li = document.createElement('li');
         li.setAttribute('key', id);
         li.id = id;
-        li.setAttribute('data-toggle', 'modal');
-        li.setAttribute('data-target', '#editModal');
+        li.setAttribute('class', 'text-dark');
 
-        li.addEventListener('click', function() {
-            document.getElementById('editModal').setAttribute('edit-id', id)
-        })
 
         let divWrapper = document.createElement('div');
         divWrapper.className = 'card border-primary mb-3';
 
         let divHeader = document.createElement('div');
-        divHeader.className = 'card-header';
+        divHeader.className = 'card-header text-dark';
 
         let divBody = document.createElement('div');
         divBody.className = 'card-body';
 
+        //Edit Button
+        let editBtn = document.createElement('button');
+        editBtn.append(document.createTextNode("Edit"));
+        editBtn.className = 'btn btn-primary float-right';
+        editBtn.id = id;
+        editBtn.setAttribute('data-toggle', 'modal');
+        editBtn.setAttribute('data-target', '#editModal');
+        // editBtn.addEventListener('click', function () {
+        //     document.getElementById('editModal').setAttribute('edit-id', id)
+        // })
+
         //Delete Button
         let deleteBtn = document.createElement('button');
         deleteBtn.append(document.createTextNode("X"));
-        deleteBtn.className = 'btn btn-primary fload right';
+        deleteBtn.className = 'btn btn-primary float-right';
         deleteBtn.id = id;
 
         //Puts them all together
         divHeader.append(chirp);
         divHeader.append(deleteBtn);
+        divHeader.append(editBtn);
         divBody.append(chirpMessage);
         divWrapper.append(divHeader);
         divWrapper.append(divBody);
-    
+
 
         li.append(divWrapper);
 
-        deleteBtn.addEventListener('click', removeChirp, false);
+        deleteBtn.addEventListener('click', removeChirp);
 
         chirpField.appendChild(li);
     }
 
-    function removeChirp(event) {
+    function removeChirp() {
         let id = this.id;
         document.getElementById(id).remove();
         $.ajax({
-            url: '/api/submit' + '/' + id,
+            url: `/api/chirps/${id}`,
             type: 'DELETE',
-            success: function() {
+            success: function () {
                 console.log('DELETED');
             }
         })
     }
 
-    submitBtn.addEventListener('click', function() {
+    submitBtn.addEventListener('click', function () {
+        event.preventDefault();
         let name = document.getElementById('name');
         let message = document.getElementById('message');
-
         postChirp(name.value, message.value);
     })
 
-    editSubmit.addEventListener('click', function() {
-      let editId = document.getElementById('editModal').getAttribute('edit-id');
-      let name = document.getElementById('editName').value;
-      let message = document.getElementById('editMessage').value;
-      $.ajax({
-          url: '/api/submit/' + editId,
-          method: 'PUT',
-          data: { name: name, message: message},
-          success: function() {
-              document.location.reload();
-          }
-      })  
-      console.log(editId)
+    editSubmit.addEventListener('click', function () {
+        let editId = document.getElementById('editModal').getAttribute('edit-id');
+        let name = document.getElementById('editName').value;
+        let message = document.getElementById('editMessage').value;
+        $.ajax({
+            url: '/api/submit/' + editId,
+            method: 'PUT',
+            data: {
+                name: name,
+                message: message
+            },
+            success: function () {
+                document.location.reload();
+            }
+        })
+        console.log(editId)
     })
 
     loadChirps();
--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const apiRoutes = require('./routes');
 const path = require('path');
 
+
 let app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, '..client')));
+app.use(express.static('client'));
 
 
 app.use('/api', apiRoutes);
